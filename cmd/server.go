ed62e82092d112905fd3432cb1183ebd33b3ebd9
zynko-dev <zynko.dev@proton.me>
2023-03-03 12:53:42 +0100

    added reload-quests command

--- cmd/server.go
+++ cmd/server.go
@@ -1,6 +1,12 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"log"
+
+	"github.com/spf13/cobra"
+	"github.com/zynko-dev/m2ctl/internal"
+)
 
 var serverCmd = cobra.Command{
 	Use:   "server",
@@ -25,6 +31,22 @@ var clearLogsCmd = cobra.Command{
 var reloadQuestsCmd = cobra.Command{
 	Use:   "reload-quests",
 	Short: "Reloads your game quests",
+	Run: func(cmd *cobra.Command, args []string) {
+		device := internal.InitSSHDevice(cmd)
+
+		if device.Debug {
+			internal.StdOutPrinter(device.Stdout)
+			internal.StdErrPrinter(device.Stderr)
+		}
+
+		files, err := internal.GetKey(fmt.Sprintf("M2CTL_FILES_%s", device.Tag))
+		if err != nil {
+			log.Fatal("Error geting profile key files", err)
+		}
+
+		device.Session.Run(internal.ServerCommands("reload-quests", files))
+		device.Session.Close()
+	},
 }
 
 func init() {
@@ -33,4 +55,7 @@ func init() {
 	serverCmd.AddCommand(&serverStopCmd)
 	serverCmd.AddCommand(&clearLogsCmd)
 	serverCmd.AddCommand(&reloadQuestsCmd)
+
+	reloadQuestsCmd.PersistentFlags().Bool("debug", false, "Set debug to true in order to see stdout and stderr on your server")
+	reloadQuestsCmd.PersistentFlags().String("tag", "", "Refrence to create profile and which data to use")
 }
